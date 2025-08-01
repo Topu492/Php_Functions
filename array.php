3dab7ac707f6c1fa4cdedcdd0d64d8f10d52c2fb
Topu492 <topu86702@gmail.com>
2025-08-01 10:58:55 +0600

    delete value from associative array

--- array.php
+++ array.php
@@ -3,6 +3,14 @@
 
 // convert string to array
  $array = explode(', ','a, b, c, d');
+                //delimeter
+
  var_dump($array);
 
 
+// delete value from associative array
+
+$person = array ('Fname' => 'Saiful','Lname'=> ' Islam');
+
+unset($person['Lname']);
+print_r($person);
